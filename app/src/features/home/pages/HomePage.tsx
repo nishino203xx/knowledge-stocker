e588a18e9cdbe063155e8d6c6e1f71bc7d1df8d0
r.nishino <nishino203xx@gmail.com>
2026-03-16 00:23:19 +0900

    feat:ホーム画面に理解記事数のヒートマップを表示（#2）

--- app/src/features/home/pages/HomePage.tsx
+++ app/src/features/home/pages/HomePage.tsx
@@ -1,9 +1,34 @@
-function HomePage() {
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
+import "../../../styles/heatmap.scss";
+import { useStudyLog } from "../../studyLog/hooks/useStudyLog";
+
+export function HomePage() {
+  const { dailyCountMap } = useStudyLog();
+  const heatmapValues = Object.entries(dailyCountMap).map(([date, count]) => ({
+    date,
+    count,
+  }));
+
   return (
     <>
-      <h1>ホーム</h1>
+      <div className="react-calendar-heatmap__scroll-container">
+        <CalendarHeatmap
+          startDate={new Date("2025-12-28")}
+          endDate={new Date("2026-12-31")}
+          showOutOfRangeDays
+          values={heatmapValues}
+          classForValue={(value) => {
+            if (!value) return "color-empty";
+            if (value.count >= 10) return "color-scale-4";
+            if (value.count >= 5) return "color-scale-3";
+            if (value.count >= 2) return "color-scale-2";
+            if (value.count >= 1) return "color-scale-1";
+
+            return "color-empty";
+          }}
+        />
+      </div>
     </>
   );
 }
-
-export default HomePage;
